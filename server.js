a8fb7737c248a909c3d8b11cb7be9562d75b8949
Robert C <robert.clarady@gmail.com>
2022-01-10 09:30:15 -0600

    fixed port error

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.get('/api/animals/:id', (req, res) => {
   });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
