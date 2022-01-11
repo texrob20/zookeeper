402433dbc2eea39811c027a7e0be38d68b6557b0
Robert C <robert.clarady@gmail.com>
2022-01-11 12:07:22 -0600

    added front end code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -109,6 +110,22 @@ app.post('/api/animals', (req, res) => {
       res.json(animal);
     }
   });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
